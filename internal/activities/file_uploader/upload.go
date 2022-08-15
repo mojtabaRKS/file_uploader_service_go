6e5d3ada8144d3b9319c48eb51f83c52bc65af35
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-15 23:01:50 +0430

    fix : add exception handling to uploader

--- internal/activities/file_uploader/upload.go
+++ internal/activities/file_uploader/upload.go
@@ -1,14 +1,19 @@
 package file_uploader
 
 import (
+	"file-uploader/internal/activities/logger"
 	"mime/multipart"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Upload(file *multipart.FileHeader, c *gin.Context) error {
-	
-	c.SaveUploadedFile(file, "storage/files")
+	err := c.SaveUploadedFile(file, "storage/files/" + file.Filename)
+
+	if err != nil {
+		logger.Logger().Error(err)
+		return err
+	}
 
 	return nil
 }
