58dc71dc5b70c426b1a07049d7ffd09d1c3e2138
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-15 22:26:59 +0430

    feat: add upload controller

--- webserver/http/controllers/v1/upload_controller.go
+++ webserver/http/controllers/v1/upload_controller.go
@@ -1,7 +1,36 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	helpers "file-uploader/internal/activities"
+	"file-uploader/internal/activities/logger"
+	workflows "file-uploader/internal/workflows"
+	v1_response "file-uploader/webserver/http/responses/v1"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func UploadController(c *gin.Context) {
-	
+
+	model, err := workflows.Upload(c)
+
+	if err != nil {
+		logger.Logger().Error(err)
+		helpers.RespondError(c, err)
+		return
+	}
+
+	response := v1_response.MainStructure{
+		Meta: v1_response.Meta{
+			Code:    http.StatusOK,
+			Message: "file_uploaded successfully",
+			Success: true,
+		},
+		Data: v1_response.UploadResponse{
+			ID:  model.ID,
+			Url: model.Path,
+		},
+	}
+
+	c.JSON(http.StatusOK, response)
 }
